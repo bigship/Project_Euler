ee3d404e412300a3c4ffeaae6e7d32268762b408
bigship <colinchen1984@gmail.com>
2013-09-12 00:33:16 +0800

    fix style, tabs to 4 spaces

--- problem02.py
+++ problem02.py
@@ -2,9 +2,9 @@
 # http://projecteuler.net/problem=2
 
 def fibonacci(limit):
-	a, b = 0, 1
-	while b < limit:
-		yield b
-		a, b = b, a+b
+    a, b = 0, 1
+    while b < limit:
+        yield b
+        a, b = b, a+b
 
 print sum(x for x in fibonacci(4000000) if x % 2 == 0)
--- problem03.py
+++ problem03.py
@@ -2,12 +2,12 @@
 # http://projecteuler.net/problem=3
 
 def is_prime(n):
-	if n < 2:
-		return False
-	for i in xrange(2, int(n**0.5)+1):
-		if n % i == 0:
-			return False
-	return True
+    if n < 2:
+        return False
+    for i in xrange(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
 
 print max(x for x in xrange(2, int(600851475143 ** 0.5) + 1) if 600851475143 % x == 0 and is_prime(x))
 
