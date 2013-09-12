b4406b316e810e83c3129dd5c42de04ebec4419e
bigship <colinchen1984@gmail.com>
2013-09-12 09:35:59 +0800

    fix style, tabs to 4 spaces

--- problem09.py
+++ problem09.py
@@ -3,10 +3,10 @@
 
 # burte force
 def solve():
-	for a in xrange(3, 1000):
-		for b in xrange(4, 1000):
-			if 1000*(a+b) - a*b == 500000:
-				c = 1000 - a - b
-				return a*b*c
+    for a in xrange(3, 1000):
+        for b in xrange(4, 1000):
+            if 1000*(a+b) - a*b == 500000:
+                c = 1000 - a - b
+                return a*b*c
 
 print solve()
--- problem10.py
+++ problem10.py
@@ -2,17 +2,17 @@
 # http://projecteuler.net/problem=10
 
 def prime_sieve(n):
-	initSieve = []
-	maxNum = int(n**0.5)+1
-	for i in xrange(2, n):
-		initSieve.append(True)
-	j = 2
-	while j <= maxNum:
-		if initSieve[j-2]:
-			for k in xrange(j*j, n, j):
-				initSieve[k-2] = False
-		j += 1
-	output = (x+2 for x in xrange(len(initSieve)) if initSieve[x])
-	return output
+    initSieve = []
+    maxNum = int(n**0.5)+1
+    for i in xrange(2, n):
+        initSieve.append(True)
+    j = 2
+    while j <= maxNum:
+        if initSieve[j-2]:
+            for k in xrange(j*j, n, j):
+                initSieve[k-2] = False
+        j += 1
+    output = (x+2 for x in xrange(len(initSieve)) if initSieve[x])
+    return output
 
 print sum(prime_sieve(2000000))
