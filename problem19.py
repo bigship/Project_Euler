0b47819198ae58479cfe91b4cda21f3bf2a29e20
bigship <colinchen1984@gmail.com>
2013-09-13 00:52:55 +0800

    add problem19

---
+++ problem19.py
@@ -0,0 +1,16 @@
+# project euler problem 19
+# http://projecteuler.net/problem=19
+
+import calendar
+
+count = 0
+for year in xrange(1901, 2001):
+    for month in xrange(1, 13):
+        if calendar.SUNDAY == calendar.weekday(year, month, 1):
+            count += 1
+
+print count
+
+# Yet another crazy one-liner :)
+print [calendar.SUNDAY == calendar.weekday(year, month, 1) for year in xrange(1901, 2001) for month in xrange(1, 13)].count(True)
+print sum([1 for year in xrange(1901, 2001) for month in xrange(1, 13) if calendar.SUNDAY == calendar.weekday(year, month, 1)])
