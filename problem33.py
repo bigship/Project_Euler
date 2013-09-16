21587ba0600244cd116339f3050051530cd0dd0b
bigship <colinchen1984@gmail.com>
2013-09-16 19:12:21 +0800

    add description

--- problem33.py
+++ problem33.py
@@ -1,6 +1,16 @@
 # project euler problem 33
 # http://projecteuler.net/problem=33
 
+'''
+DESCRIPTION
+The fraction 49/98 is a curious fraction, as an inexperienced mathematician in attempting to simplify 
+it may incorrectly believe that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
+We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
+There are exactly four non-trivial examples of this type of fraction, less than one in value, and containing 
+two digits in the numerator and denominator.
+If the product of these four fractions is given in its lowest common terms, find the value of the denominator.
+'''
+
 def gcd(a, b):
     if a < b:
         a, b = b, a
@@ -36,6 +46,4 @@ for a in xrange(11, 100):
                     denominator.append(b)
 
 print numerator, denominator 
-print reduce(lambda x,y:x*y, denominator) / gcd(reduce(lambda x, y:x*y, numerator), reduce(lambda x,y:x*y, denominator))
-
-
+print reduce(lambda x,y:x*y, denominator) / gcd(reduce(lambda x,y:x*y, numerator), reduce(lambda x,y:x*y, denominator))
