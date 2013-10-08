847c8e155660291a94618929e48550ac975bc037
bigship <colinchen1984@gmail.com>
2013-10-08 16:13:30 +0800

    add problem64

---
+++ problem64.py
@@ -0,0 +1,31 @@
+# project euler problem 64
+# http://projecteuler.net/problem=64
+
+upperbound = 10000
+cnt = 0
+
+for n in xrange(2, upperbound + 1):
+    limit = int(n**0.5)
+    if limit ** 2 == n:
+        continue
+
+    period = 0
+    d = 1
+    m = 0
+    a = limit
+
+    m = d*a - m
+    d = (n - m**2) / d
+    a = (limit + m) / d
+    period += 1
+
+    while a != 2*limit:
+        m = d*a - m
+        d = (n - m**2) / d
+        a = (limit + m) / d
+        period += 1
+
+    if period % 2 == 1:
+        cnt += 1
+
+print cnt 
